2e865f053ee5db4a29065cbbdae217176e9f2b0a
ramses-ayala <ramses0595@gmail.com>
2023-11-07 13:08:54 -0600

    init graphql server

---
+++ db/_db.js
@@ -0,0 +1,27 @@
+
+export const games = [
+    {id: '1', title: 'something 1', platform: ['Switch', 'Xbox']},
+    {id: '2', title: 'something 2', platform: ['PC']},
+    {id: '3', title: 'something 3', platform: ['PS5', 'PC']},
+    {id: '4', title: 'something 4', platform: ['Switch']},
+    {id: '5', title: 'something 5', platform: ['Xbox']},
+];
+
+export const authors = [
+    {id: '1', name: 'gus', verified: false},
+    {id: '2', name: 'janneth', verified: true},
+    {id: '3', name: 'teacher johana', verified: false},
+];
+
+export const reviews = [
+    {id: '1', rating: 9, content: 'lorem ipsum', author_id: 1, game_id: 1},
+    {id: '2', rating: 2, content: 'lorem ipsum', author_id: 2, game_id: 4},
+    {id: '3', rating: 5, content: 'lorem ipsum', author_id: 3, game_id: 5},
+    {id: '4', rating: 7, content: 'lorem ipsum', author_id: 2, game_id: 2},
+    {id: '5', rating: 10, content: 'lorem ipsum', author_id: 3, game_id: 3},
+    {id: '6', rating: 1, content: 'lorem ipsum', author_id: 1, game_id: 4},
+    {id: '7', rating: 10, content: 'lorem ipsum', author_id: 2, game_id: 1},
+    {id: '8', rating: 6, content: 'lorem ipsum', author_id: 3, game_id: 2},
+    {id: '9', rating: 7, content: 'lorem ipsum', author_id: 2, game_id: 5},
+    {id: '10', rating: 3, content: 'lorem ipsum', author_id: 1, game_id: 3},
+];
---
+++ index.js
@@ -0,0 +1,40 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+import { typeDefs } from './schema/schema.js';
+import { games, authors, reviews } from './db/_db.js';
+
+const resolvers = {
+    Query: {
+        authors(){
+            return authors
+        },
+        author(_, args){
+            return authors.find((author) => author.id === args.id);
+        },
+        games(){
+            return games
+        },
+        game(_, args){
+            return games.find((game) => game.id === args.id);
+        },
+        reviews(){
+            return reviews
+        },
+        review(_, args){
+            return reviews.find((review) => review.id === args.id);
+        }
+    }
+}
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+const {url} = startStandaloneServer(server, {
+    listen: {port: 4000}
+});
+
+console.log(`Server on port : `, 4000);
